04c53106a94451f598055caa75234e125f20dd9b
Harold Steven Taborda Gutiérrez <htaborda@heinsohn.com.co>
2018-04-06 15:32:01 -0500

    Estilos

--- faceleApp/src/pages/home/home.ts
+++ faceleApp/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private navCtrl: NavController) {
   }
+  
+  private iniciar(_credentials) {
+      console.log("Inicia sesion");
+       
+      }
+     
+     
+private crear(_credentials) {
+    console.log("crea usuario");
+      
+      }
+      
+      private recordar(_credentials) {
+    console.log("Olvido contraseña");
+      
+      }
 
 }
